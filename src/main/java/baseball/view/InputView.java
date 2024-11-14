281dccdd0c6af128b2816d43c8a14d7dd12e4251
sjmmics <sjmmics@gmail.com>
2024-11-15 08:59:40 +0900

    feat : ($InputView) 입력값 내용 및 형식을 안내하고 입력값을 받는 기능 추가
    printStart() : 게임 시작 메시지 출력
    
    getUserBall() : 사용자로부터 숫자 3개를 입력하라고 안내 후 입력받은 값을 반환
    
    getPlayAgain() : 게임 종료 메시지 출력 후 다시 게임을 시작할지 입력 받고 반환

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,21 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public void printStart() {
+        System.out.println(IOMessage.GAME_START);
+    }
+
+    public String getUserBall() {
+        System.out.print(IOMessage.ENTER_BALL);
+        return Console.readLine();
+    }
+
+    public String getPlayAgain() {
+        System.out.println(IOMessage.PLAY_ANOTHER);
+        return Console.readLine();
+    }
+
+}

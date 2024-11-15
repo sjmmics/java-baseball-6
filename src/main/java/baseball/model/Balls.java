7c7287a6a2efaeb44d75f7af323e87bcd313203c
sjmmics <sjmmics@gmail.com>
2024-11-15 09:17:42 +0900

    feat : ($Balls) 사용자 입력값에 따라 입력형식 검증한 다음 숫자 3개 정하는 기능 추
    검증 : 형식, 내용 검증으로 분류. 다음 경우에 해당하는 경우 IllegalException 호출.
    형식 검증 : 입력값이 비었을 경우, 입력값에 빈칸(띄어쓰기)가 포함된 경우, 입력값 길이가 3이 아닌 경우, 입력값에 숫자가 아닌 문자가 있을 경우
    내용 검증 : 숫자가 1 ~ 9사이 수가 아닌 경우, 숫자값이 중복된 경우

--- src/main/java/baseball/model/Balls.java
+++ src/main/java/baseball/model/Balls.java
@@ -3,6 +3,8 @@ package baseball.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,6 +16,8 @@ public class Balls {
     public final static int MAX_BALL_NUMBER = 9;
 
     public final static int BALL_COUNT = 3;
+
+    private final static String BLANK = " ";
 
     private final List<Integer> balls;
 
@@ -29,6 +33,69 @@ public class Balls {
         }
         List<Integer> balls = new ArrayList<>(ballsNotDuplicate);
         return new Balls(balls);
+    }
+
+    public static Balls getFromUserInput(String line) {
+        validateFormat(line);
+        validateContent(line);
+        String[] lineSplit = line.split("");
+        List<Integer> balls = Arrays.stream(lineSplit)
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList();
+        return new Balls(balls);
+    }
+
+    private static void validateFormat(String line) throws IllegalArgumentException {
+        if (line.isEmpty() || line.contains(BLANK)) {
+            throw new IllegalArgumentException();
+        }
+        if (line.length() != BALL_COUNT) {
+            throw new IllegalArgumentException();
+        }
+        if (doesContainsNotDigit(line)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean doesContainsNotDigit(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            char ball = line.charAt(i);
+            if (!Character.isDigit(ball)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void validateContent(String line) throws IllegalArgumentException {
+        if (doesOutOfBallNumberRange(line)) {
+            throw new IllegalArgumentException();
+        }
+        if (doesContainsDuplicateNumber(line)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean doesOutOfBallNumberRange(String line) {
+        String[] lineSplit = line.split("");
+        for (int i = 0; i < line.length(); i++) {
+            int ball = Integer.parseInt(lineSplit[i]);
+            if (ball < MIN_BALL_NUMBER|| MAX_BALL_NUMBER < ball) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean doesContainsDuplicateNumber(String line) {
+        String[] lineSplit = line.split("");
+        List<Integer> ballsMayDuplicate = Arrays.stream(lineSplit)
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList();
+        Set<Integer> ballsNotDuplicate = new HashSet<>(ballsMayDuplicate);
+        return ballsMayDuplicate.size() != ballsNotDuplicate.size();
     }
 
 }

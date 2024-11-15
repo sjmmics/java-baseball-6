7ade2dbffb3a0fd49feaf857d4366990aa75437f
sjmmics <sjmmics@gmail.com>
2024-11-15 09:34:10 +0900

    feat : ($Application) 프로그램 시작 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.appconfig.AppConfig;
+import baseball.controller.BaseballController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig config = new AppConfig();
+        BaseballController controller = config.getBaseballController();
+        controller.run();
     }
 }
